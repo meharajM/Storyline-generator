92c347a32860f34abaa4bc7d3b228b1afc1ab2b6
meharaj.moulasab <meharaj.moulasab@automationanywhere.com>
2021-01-27 23:54:00 +0530

    Updates

---
+++ precache-manifest.d5c13656cde26d68d0046595f8995cda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a8db8c96871157e666561a29900a58",
+    "url": "/Storyline-generator/index.html"
+  },
+  {
+    "revision": "5369411b2ca76b0e569d",
+    "url": "/Storyline-generator/static/css/2.8c1dcbfa.chunk.css"
+  },
+  {
+    "revision": "f7b1b99579886559e87d",
+    "url": "/Storyline-generator/static/css/main.1f3e683e.chunk.css"
+  },
+  {
+    "revision": "5369411b2ca76b0e569d",
+    "url": "/Storyline-generator/static/js/2.657a159d.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/Storyline-generator/static/js/2.657a159d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b1b99579886559e87d",
+    "url": "/Storyline-generator/static/js/main.6dc89c63.chunk.js"
+  },
+  {
+    "revision": "39e33ef13a36d85f8059",
+    "url": "/Storyline-generator/static/js/runtime-main.0b038d49.js"
+  }
+]);
